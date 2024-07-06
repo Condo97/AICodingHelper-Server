dd4fbf9f4bca823ae3d6db86ee40b894ed7352a5
Alex C <acoundou@gmail.com>
2024-07-06 19:40:59 -0400

    Wow nice lookin like it's working!

--- lib/src/main/java/com/aicodinghelper/openai/functioncall/PlanCodeGenerationFC.java
+++ lib/src/main/java/com/aicodinghelper/openai/functioncall/PlanCodeGenerationFC.java
@@ -22,19 +22,19 @@ public class PlanCodeGenerationFC {
         @FCParameter(description = "only for edit, the summary of the edits to be made, for use when prompting GPT with the changes that should be made", required = false)
         private String edit_summary;
 
-        @FCParameter(description = "only for edit, the files necessary to reference from the included filemap or created files")
-        private List<String> edit_reference_filepaths;
+        @FCParameter(description = "only for edit, the files necessary to reference from the included filemap or created files", required = false)
+        private List<String> reference_filepaths;
 
         public Step() {
 
         }
 
-        public Step(Integer index, String action, String filepath, String edit_summary, List<String> edit_reference_filepaths) {
+        public Step(Integer index, String action, String filepath, String edit_summary, List<String> reference_filepaths) {
             this.index = index;
             this.action = action;
             this.filepath = filepath;
             this.edit_summary = edit_summary;
-            this.edit_reference_filepaths = edit_reference_filepaths;
+            this.reference_filepaths = reference_filepaths;
         }
 
         public Integer getIndex() {
@@ -53,8 +53,8 @@ public class PlanCodeGenerationFC {
             return edit_summary;
         }
 
-        public List<String> getEdit_reference_filepaths() {
-            return edit_reference_filepaths;
+        public List<String> getReference_filepaths() {
+            return reference_filepaths;
         }
 
     }
